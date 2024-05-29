35a8bf8b9ab8af2cf3865816cd6bb05448f3943b
ZhouYHZ <110000082+ZhouYHZ@users.noreply.github.com>
2024-05-29 14:46:32 +0930

    v 0.28

--- index.js
+++ index.js
@@ -79,18 +79,8 @@ function myTimer () {
         }
         let sec = i - m * 60;
         document.getElementsByClassName("time")[0].innerHTML = m + ":" + sec;
-        document.getElementsByClassName("time")[1].innerHTML = m + ":" + sec;
-        document.getElementsByClassName("time")[2].innerHTML = m + ":" + sec;
-        document.getElementsByClassName("time")[3].innerHTML = m + ":" + sec;
-        document.getElementsByClassName("time")[4].innerHTML = m + ":" + sec;
-        document.getElementsByClassName("time")[5].innerHTML = m + ":" + sec;
     } else {
         document.getElementsByClassName("time")[0].innerHTML = i;
-        document.getElementsByClassName("time")[1].innerHTML = i;
-        document.getElementsByClassName("time")[2].innerHTML = i;
-        document.getElementsByClassName("time")[3].innerHTML = i;
-        document.getElementsByClassName("time")[4].innerHTML = i;
-        document.getElementsByClassName("time")[5].innerHTML = i;
     }
     i = i + 1;
 };
